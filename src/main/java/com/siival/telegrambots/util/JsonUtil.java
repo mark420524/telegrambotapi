5973aa0dd174e2adb9fae96a6c98a5d4e2382fc0
mark <markjava@foxmail.com>
2022-01-07 18:02:25 +0800

    增加json序列化处理问题

--- src/main/java/com/siival/telegrambots/util/JsonUtil.java
+++ src/main/java/com/siival/telegrambots/util/JsonUtil.java
@@ -2,8 +2,10 @@ package com.siival.telegrambots.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.siival.telegrambots.resp.SendMessageResponse;
 
 public class JsonUtil {
 
@@ -26,6 +28,7 @@ public class JsonUtil {
         }
         ObjectMapper objectMapper = new ObjectMapper();
         try {
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             JsonNode respNode = objectMapper.readTree(json);
             return objectMapper.convertValue(respNode, clazz);
         } catch (JsonProcessingException e) {
@@ -33,4 +36,6 @@ public class JsonUtil {
         }
         return null;
     }
+
+
 }

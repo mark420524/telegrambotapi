04d9519b322e308f408577d55eadebc88d2f0a7d
mark <markjava@foxmail.com>
2022-01-11 17:36:57 +0800

    优化数据结构

--- src/main/java/com/siival/telegrambots/resp/GetMeResponse.java
+++ src/main/java/com/siival/telegrambots/resp/GetMeResponse.java
@@ -1,22 +1,11 @@
 package com.siival.telegrambots.resp;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.siival.telegrambots.core.types.User;
 import lombok.Data;
 
 @Data
 public class GetMeResponse extends BaseResponse {
 
-    private ResponseResult result;
+    private User result;
 
-    @Data
-    private class ResponseResult {
-        private Long id;
-        @JsonProperty("is_bot")
-        private boolean  bot;
-        private String first_name;
-        private String username;
-        private boolean  can_join_groups;
-        private boolean can_read_all_group_messages;
-        private boolean supports_inline_queries;
-    }
 }

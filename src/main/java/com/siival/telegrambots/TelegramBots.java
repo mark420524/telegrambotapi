4c7ce846acf0121c9f3211a79ba2730b7c5e88bb
mark <markjava@foxmail.com>
2022-01-13 22:02:20 +0800

    增加注释

--- src/main/java/com/siival/telegrambots/TelegramBots.java
+++ src/main/java/com/siival/telegrambots/TelegramBots.java
@@ -2,7 +2,12 @@ package com.siival.telegrambots;
 
 import com.siival.telegrambots.enums.MethodEnum;
 import com.siival.telegrambots.resp.BaseResponse;
-
+/**
+ * @author mark acrossxwall@gmail.com
+ * @description bot interface
+ * @date 2022/1/13 22:00
+ * @version  1.0.0
+ */
 public interface TelegramBots<T extends BaseResponse> {
 
     T callMethod(MethodEnum method, Object params) throws  Exception  ;
